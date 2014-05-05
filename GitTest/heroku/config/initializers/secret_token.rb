57fb046008c5a1c12200509f9136e7ae8a0d64c4
Prateep Kul <1.0@kul.com>
2014-05-05 09:53:13 +0700

    Initial Test

---
+++ GitTest/heroku/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Heroku::Application.config.secret_token = '96a6dfcac55028a7233951ff2d2d817de552b0ec81be37d33a22d8cb4c4f8bcf0fa027718c4d3c237fc93de2d1c8e35867978f196ce539f562b1a6eda22fc7b5'
